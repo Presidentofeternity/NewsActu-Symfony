c7c4acb5d304ce59b34f42a0f817687fd732b75b
xavier <xaviermahut@gmail.com>
2022-03-17 12:28:52 +0100

    add posts to home + create swho articlre

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,9 +13,14 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="default_home", methods={"GET"})
      */
-    public function home(): Response
+    public function home(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('default/home.html.twig');
+
+        $articles = $entityManager->getRepository(Article::class)->findBy(['deletedAt' => null]);
+
+        return $this->render('default/home.html.twig', [
+            'articles' => $articles,
+        ]);
     }
 
 }
